0edaaf740288c120a8fdadc4b425a6477d56bcee
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2022-02-23 01:41:25 -0300

    Resolución del desafío

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 const cosasQueAprendimos = [
-  {
-    tema: "terminal",
-    class: "",
-  },
-  {
-    tema: "node",
-    class: "",
-  },
-  {
-    tema: "oop",
-    class: "",
-  },
-  {
-    tema: "typescript",
-    class: "",
-  },
-  {
-    tema: "css",
-    class: "",
-  },
-  {
-    tema: "dom",
-    class: "special",
-  },
+	{
+		tema: "terminal",
+		class: "",
+	},
+	{
+		tema: "node",
+		class: "",
+	},
+	{
+		tema: "oop",
+		class: "",
+	},
+	{
+		tema: "typescript",
+		class: "",
+	},
+	{
+		tema: "css",
+		class: "",
+	},
+	{
+		tema: "dom",
+		class: "special",
+	},
 ];
 
-function main() {}
+function main() {
+	const listaEl = document.querySelector(".lista");
+	const liListaEl = document.querySelectorAll(".lista li");
+
+	liListaEl.forEach((li) => li.remove());
+
+	cosasQueAprendimos.forEach((li) => {
+		const listItemEl = document.createElement("li");
+		listItemEl.textContent = `Tema: ${li["tema"]}`;
+		listItemEl.className = li["class"];
+		return listaEl.appendChild(listItemEl);
+	});
+}
 
 main();
